d73bc6ead9e799fa5d73cda8183e11467b69514a
danehans <danehans@cisco.com>
2013-05-21 21:00:20 +0000

    added qemu.conf config file requirement when using OVS libvirt_vif_driver

--- manifests/compute/quantum.pp
+++ manifests/compute/quantum.pp
@@ -6,4 +6,14 @@ class nova::compute::quantum (
     'DEFAULT/libvirt_vif_driver':             value => $libvirt_vif_driver;
     'DEFAULT/libvirt_use_virtio_for_bridges': value => 'True';
   }
+
+  if $libvirt_vif_driver == 'nova.virt.libvirt.vif.LibvirtOpenVswitchDriver' {
+    Package['libvirt'] ->
+    file_line { 'quemu_hack':
+      line        => 'cgroup_device_acl = ["/dev/null", "/dev/full", "/dev/zero", "/dev/random", "/dev/urandom", "/dev/ptmx", "/dev/kvm", "/dev/kqemu", "/dev/rtc", "/dev/hpet", "/dev/net/tun",]',
+      path        => '/etc/libvirt/qemu.conf',
+      ensure      => present,
+    } ~> Service['libvirt']
+  }
+
 }
